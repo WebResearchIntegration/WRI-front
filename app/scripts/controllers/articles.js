6849f0fc46351a9764db0a7f5dc69dcef8185d74
Marc <marc.e.habib@gmail.com>
2017-10-27 15:43:55 +0200

    init list articles

---
+++ app/scripts/controllers/articles.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wriApp.controller:ArticlesCtrl
+ * @description
+ * # ArticlesCtrl
+ * Controller of the wriApp
+ */
+angular.module('wriApp')
+    .controller('ArticlesCtrl', function ($scope, articleService) {
+        
+        // var promise = articleService.getArticles();
+        // promise.then(function (data){
+        //     $scope.articles = data;
+        // })
+    });
