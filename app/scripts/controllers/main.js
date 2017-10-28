df31a7d3ff7be35e623fe5b7bb0c52b5d8152878
Hug0xynum <hugo@oxynum.fr>
2017-10-28 03:25:05 +0200

    test all templates system for element-viewer

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,25 @@
  * Controller of the wriApp
  */
 angular.module('wriApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', ['$scope', function ($scope) {
 
-  });
+    $scope.selectedElement = {
+      status : {
+        saved: false,
+        printed: false,
+        read: false
+      },
+      title: "Donald Duck and the Golden Rain",
+      score: 443,
+      conference: "City Of Disneyland",
+      date: Date.now(),
+      authors_ids: [4, 21, 18],
+      keywords: ["#disney", "#duck", "#gold"],
+      abstract: "Donald Duck discover some gold",
+      description: "Donald Duck was relaxing when a golden meteor dropped off in his garden ...",
+      references_ids: [],
+      notes_ids: [12, 34, 56]
+    };
+
+    $scope.selectedElementType = "article";
+  }]);
