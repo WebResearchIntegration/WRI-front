a4e576be0bd5419aad6864350a1d076b2f59185e
Simbadev <maxime.gasnier.1@gmail.com>
2017-11-13 18:35:52 +0100

    add authors, questions services

---
+++ app/scripts/services/authors.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name wriApp.authors
+ * @description
+ * # authors
+ * Service in the wriApp.
+ */
+angular.module('wriApp')
+  .service('authors', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ app/scripts/services/questions.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name wriApp.questions
+ * @description
+ * # questions
+ * Service in the wriApp.
+ */
+angular.module('wriApp')
+  .service('questions', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
