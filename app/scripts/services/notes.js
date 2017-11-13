1435ea3f238a2691303c72a86da4d39b53493295
Simbadev <maxime.gasnier.1@gmail.com>
2017-11-13 18:34:10 +0100

    generate notes service

---
+++ app/scripts/services/notes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name wriApp.notes
+ * @description
+ * # notes
+ * Service in the wriApp.
+ */
+angular.module('wriApp')
+  .service('notes', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
