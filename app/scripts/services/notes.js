559a55795fe68260e62a4b4ae3b3b728c189e147
Simbadev <maxime.gasnier.1@gmail.com>
2017-11-14 17:13:11 +0100

    add basic structure notes

--- app/scripts/services/notes.js
+++ app/scripts/services/notes.js
@@ -10,6 +10,33 @@
 angular.module('wriApp')
   .service('notesService', function () {
     var service = this;
+    
+    /**
+     * 
+     */
+    service.getAll = function () {
+      return Restangular.all('article').getList();
+    };
 
+    /**
+     * 
+     */
+    service.getById = function (id) {
+
+    };
+
+    /**
+     * 
+     */
+    service.updateById = function (id) {
+
+    }
+
+    /**
+     * 
+     */
+    service.delete = function (id) {
+
+    }
     return service;
   });
