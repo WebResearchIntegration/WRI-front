0455b922e64d2f164e9dc3fd0e6d28b387a956a6
Simbadev <maxime.gasnier.1@gmail.com>
2017-11-14 17:12:43 +0100

    add basic strcture authors

--- app/scripts/services/authors.js
+++ app/scripts/services/authors.js
@@ -11,5 +11,33 @@ angular.module('wriApp')
   .service('authorsService', function () {
     var service = this;
 
+    /**
+     * 
+     */
+    service.getAll = function () {
+      return Restangular.all('article').getList();
+    };
+
+    /**
+     * 
+     */
+    service.getById = function (id) {
+
+    };
+
+    /**
+     * 
+     */
+    service.updateById = function (id) {
+
+    }
+
+    /**
+     * 
+     */
+    service.delete = function (id) {
+
+    }
+    
     return service;
   });
