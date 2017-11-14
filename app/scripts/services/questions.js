f92aef28a7cd8f4918ec5955175bd87d9c9e893f
Simbadev <maxime.gasnier.1@gmail.com>
2017-11-14 17:13:41 +0100

    add basic structure questions

--- app/scripts/services/questions.js
+++ app/scripts/services/questions.js
@@ -11,5 +11,32 @@ angular.module('wriApp')
   .service('questionsService', function () {
     var service = this;
 
+    /**
+     * 
+     */
+    service.getAll = function () {
+      return Restangular.all('article').getList();
+    };
+
+    /**
+     * 
+     */
+    service.getById = function (id) {
+
+    };
+
+    /**
+     * 
+     */
+    service.updateById = function (id) {
+
+    }
+
+    /**
+     * 
+     */
+    service.delete = function (id) {
+
+    }
     return service;
   });
