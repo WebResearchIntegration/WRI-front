377c75e1001814bd4b9e993fdee6a60edf2da9e7
Simbadev <maxime.gasnier.1@gmail.com>
2017-11-17 02:09:56 +0100

    add comments inside elementViewer.js to complete documentation

--- app/scripts/directives/elementviewer.js
+++ app/scripts/directives/elementviewer.js
@@ -31,7 +31,8 @@ angular.module('wriApp')
         };
 
         /**
-         * 
+         * Will toggle the status of the selected element.
+         * @param {String} status value that corresponds to the selected status can be 'saved', 'read' or 'printed'
          */
         $scope.toggleStatus = function(status){
           $scope.element.status[status] = !$scope.element.status[status];
@@ -39,28 +40,34 @@ angular.module('wriApp')
         };
 
         /**
-         * 
+         * Will check if the element is an array.
+         * @param {Object} elementToAnalyze the sending object to compare.
+         * @return {boolean} true if element is array, false if it's not.
          */
         $scope.isArray = function(elementToAnalyze) {
           return Array.isArray(elementToAnalyze);
         }
 
         /**
-         * 
+         * Will watch the value to know if the viewer is open.
          */
         $scope.$watch('isViewerOpen', function(old) {
           console.log(old)
         })
 
         /**
-         * 
+         * Will send a message to parent scoep or rootScope
+         * to prevent of a closeViewer
+         * @event closeViewer
          */
         $scope.closePanel =  function() {
           $scope.$emit('closeViewer');
         }
 
         /**
-         * 
+         * Will send a message to parent scope or rootScope
+         * to prevent of a enableReferenceModeOn
+         * @event enableReferenceModeOn
          */
         $scope.sendEnableReferenceEdition = function() {
           $scope.$emit('enableReferenceModeOn');
