8cfaffd3f30beb36d877cc1cdd2d11bc64eb1a9f
Simbadev <maxime.gasnier.1@gmail.com>
2018-01-21 20:24:04 +0100

    generate controller of login Page and the view it belongs to

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wriApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the wriApp
+ */
+angular.module('wriApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
