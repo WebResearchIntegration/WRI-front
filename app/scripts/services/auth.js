31bdafb08ed973b90bfd4b25ee4a6219785466b9
Simbadev <maxime.gasnier.1@gmail.com>
2018-01-21 20:54:56 +0100

    generate auth service

---
+++ app/scripts/services/auth.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name wriApp.Auth
+ * @description
+ * # Auth
+ * Service in the wriApp.
+ */
+angular.module('wriApp')
+  .service('Auth', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
